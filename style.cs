bc0a3e05d30dc77cb38c91c93d0c6ba77f27bffe
Clint McCalla <clint.mccalla@gmail.com>
2017-04-13 12:25:49 -0500

    Create style.cs

---
+++ style.cs
@@ -0,0 +1,52 @@
+body {
+  font-family: "Vollkorn", serif; /*revisit this*/
+  font-size: 16px;
+  background-color: #FFAC7D;/*insert background color here*/
+  background-image: url("https://ihunsw.edu.au/wp-content/uploads/2014/07/grey-website-background-illusionapparel2.jpg")
+}
+
+.jumbotron {
+  width: 50%;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 15px;
+  border: 1px solid black;
+  border-radius: 20px;
+  background-color: ;
+  box-shadow: 6px 6px;
+  padding-top: 15px;
+}
+
+footer {
+  background-color: white;
+  width: 50%;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 10px;
+  border: 1px solid black;
+  border-radius: 20px;
+  background-color: ;
+  box-shadow: 6px 6px;
+  padding-top: 5px;
+  padding-bottom: 5px;
+}
+
+h1 { 
+font-size: 2.2em;
+}
+
+button {
+  font-family: "Vollkorn", serif;
+  font-size: 1.1em;
+  border-radius: 5px;
+  margin-left: 35%;
+  margin-right: 20px;
+}
+
+.btn-twitter, .fa-twitter {
+  color: white;
+  background-color: #55ACEE;
+  margin-left: auto;
+  margin-right: auto;
+  font-size: 0.5em;
+}
